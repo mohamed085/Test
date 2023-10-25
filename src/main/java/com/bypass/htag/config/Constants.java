81ad346539c466ceb307476a994d15156ee6e161
Mohamed <mohamed.emad@bypa-ss.com>
2023-10-25 15:43:45 +0300

    Initial version of providerService generated by JHipster-7.0.1

---
+++ src/main/java/com/bypass/htag/config/Constants.java
@@ -0,0 +1,11 @@
+package com.bypass.htag.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
---
+++ src/main/java/com/bypass/htag/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.bypass.htag.security;
